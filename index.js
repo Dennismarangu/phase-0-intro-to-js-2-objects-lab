f49afb8db2d6e4dbff6edebaf74d5aa392e67b0c
+ <+>
2023-03-11 17:42:18 +0300

    Not yet complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Dennis',
+    streetAddress: '0100',
+}
+
+const employee2 = { name: 'John', age: 30, position: 'Manager' };
+const updatedEmployee2 = ('Dennis', 'age', 31);
+console.log(updatedEmployee2);
+
+console.log(employee2);
+
+const employee3 = { name: 'John', age: 30, position: 'Manager' };
+employee2('John', 'age', 31);
+
+console.log(employee2);
+
+function deleteFromEmployee3(John, key) {
+  const { [key]: deletedKey, ...rest } = John;
+  return rest;
+}
+
+function destructivelyDeleteFromEmployee3(John, key) {
+  delete John[key];
+  return John;
+}
